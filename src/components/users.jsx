1fee8daff30d4671d69e6a31dc27ca9dbb506fc7
AlexLenk <alexlenk.ru@gmail.com>
2022-11-12 15:34:00 +0300

    Created a users component

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,18 +1,68 @@
 import React, {useState} from 'react'
 import api from '../api'
+import declensionWords from '../utils/declensionWords'
 
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll())
 
   const handleDelete = (userId) => {
+    setUsers(users.filter(user => user._id !== userId))
   }
 
   const renderPhrase = (number) => {
+    return `${number} человек${declensionWords(number, ['', 'а', ''])} тусуются с тобою сегодня`
   }
 
-  return (
-    <div>Title</div>
-  )
+  const renderUsers = () => {
+    return users.map(user => (
+      <tr key={user._id}>
+        <td>{user.name}</td>
+        <td>
+          {user.qualities.map(quality => (
+            <span className={`badge m-1 bg-${quality.color}`} key={quality._id}>
+              {quality.name}
+            </span>
+          ))}
+        </td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate}</td>
+        <td>
+          <button className="btn-danger btn" onClick={() => handleDelete(user._id)}>удалить</button>
+        </td>
+      </tr>
+    ))
+  }
+
+  return <div className="container pt-5 pb-2">
+    {!users.length ? (
+      <div className="mb-5 alert-danger alert">Никто не тусуются с тобою сегодня</div>
+    ) : (
+      <>
+        <div className="mb-5 alert-primary alert">
+          {renderPhrase(users.length)} человек тусуются с тобою сегодня
+        </div>
+
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">&nbsp;</th>
+            </tr>
+            </thead>
+            <tbody>
+            {renderUsers()}
+            </tbody>
+          </table>
+        </div>
+      </>
+    )}
+  </div>
 }
 
 export default Users
---
+++ src/utils/declensionWords.js
@@ -0,0 +1,10 @@
+const declensionWords = (number, words) => {
+  const cases = [2, 0, 1, 1, 1, 2]
+  return words[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+    ]
+}
+
+export default declensionWords

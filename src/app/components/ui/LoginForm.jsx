306fbe34abe5f07815e2cc6e67b651e3591092ee
Alex Lenk <alexlenk.ru@gmail.com>
2023-02-26 14:27:42 +0300

    Created login form authorization (#12)

--- src/app/components/ui/LoginForm.jsx
+++ src/app/components/ui/LoginForm.jsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react'
+import {useHistory} from 'react-router-dom'
 import TextField from '../common/form/TextField'
 import CheckBoxField from '../common/form/CheckboxField'
 import {validator} from '../../utils/validator'
 import {validatorConfig} from '../../utils/validatorConfig'
+import {useAuth} from '../../hooks/useAuth'
+import {toast} from 'react-toastify'
 
 const LoginForm = () => {
   const [data, setData] = useState({
@@ -10,13 +13,17 @@ const LoginForm = () => {
     password: '',
     stayOn: false
   })
+  const history = useHistory()
+  const {logIn} = useAuth()
   const [errors, setErrors] = useState({})
+  const [enterError, setEnterError] = useState(null)
 
   const handleChange = (target) => {
     setData(prevState => ({
       ...prevState,
       [target.name]: target.value
     }))
+    setEnterError(null)
   }
 
   useEffect(() => {
@@ -29,37 +36,59 @@ const LoginForm = () => {
   }
   const isValid = Object.keys(errors).length === 0
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     const isValid = validate()
     if (!isValid) return
-    console.log(data)
+
+    try {
+      await logIn(data)
+
+      toast.success('Вы успешно авторизовались и будите перенаправлены на главную страницу через 2 секунды', {
+        position: 'top-center',
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+
+      setTimeout(() => {
+        history.push('/')
+      }, 2000)
+    } catch (error) {
+      setEnterError(error.message)
+    }
   }
   return (
     <form onSubmit={handleSubmit}>
       <TextField
-        label='Email'
-        name='email'
+        label="Электронная почта"
+        name="email"
         value={data.email}
         onChange={handleChange}
         error={errors.email}
       />
 
       <TextField
-        label='Пароль'
-        type='password'
-        name='password'
+        label="Пароль"
+        type="password"
+        name="password"
         value={data.password}
         onChange={handleChange}
         error={errors.password}
       />
+
       <CheckBoxField
         value={data.stayOn}
         onChange={handleChange}
-        name='stayOn'
+        name="stayOn"
       >
         Оставаться в системе
       </CheckBoxField>
+
+      {enterError && <p className="text-danger">{enterError}</p>}
 
       <button
         className="btn btn-primary w-100 mx-auto"

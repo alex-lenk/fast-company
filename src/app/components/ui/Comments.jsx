dab472a7ba74193420c179db0b1b2edd0474ffb5
AlexLenk <alexlenk.ru@gmail.com>
2023-03-19 18:02:01 +0300

    Change comments and final fixed project

--- src/app/components/ui/Comments.jsx
+++ src/app/components/ui/Comments.jsx
@@ -1,14 +1,17 @@
 import {orderBy} from 'lodash'
 import React, {useEffect} from 'react'
 import CommentsList, {AddCommentForm} from '../common/comments'
-import {useComments} from '../../hooks/useComments'
 import {useDispatch, useSelector} from 'react-redux'
 import {
   getComments,
   getCommentsLoadingStatus,
-  loadCommentsList
+  loadCommentsList,
+  createComment,
+  removeComment,
 } from '../../store/comments'
 import {useParams} from 'react-router-dom'
+import {nanoid} from 'nanoid'
+import {getCurrentUserId} from '../../store/users'
 
 const Comments = () => {
   const {userId} = useParams()
@@ -17,15 +20,25 @@ const Comments = () => {
     dispatch(loadCommentsList(userId))
   }, [userId])
   const isLoading = useSelector(getCommentsLoadingStatus())
-  const {createComment, removeComment} = useComments()
   const comments = useSelector(getComments())
+  const currentUserId = useSelector(getCurrentUserId())
 
   const handleSubmit = (data) => {
-    createComment(data)
+    const comment = {
+      ...data,
+      _id: nanoid(),
+      pageId: userId,
+      created_at: Date.now(),
+      userId: currentUserId,
+    }
+
+    dispatch(createComment(comment))
   }
+
   const handleRemoveComment = (id) => {
-    removeComment(id)
+    dispatch(removeComment(id))
   }
+
   const sortedComments = orderBy(comments, ['created_at'], ['desc'])
 
   return (
